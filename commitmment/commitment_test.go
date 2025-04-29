cc7d949b3d9fe0709ffad74cb088fa3cc28de0cf
Felix Prasanna <fpx@mit.edu>
2025-04-29 16:56:27 -0400

    Add negative test

--- commitmment/commitment_test.go
+++ commitmment/commitment_test.go
@@ -7,11 +7,20 @@ import (
 	"github.com/fprasx/secrets-and-spies/utils"
 )
 
-func Test1(t *testing.T) {
+func TestSuccess(t *testing.T) {
 	message := uint(0xdecaf)
 	commitment, nonce := commitmment.Commit(message)
 	utils.Assert(
 		commitmment.Verify(commitment, message, nonce),
 		"failed to verify commitmment",
+	)
+}
+
+func TestFail(t *testing.T) {
+	message := uint(0xdecaf)
+	commitment, nonce := commitmment.Commit(message)
+	utils.Assert(
+		!commitmment.Verify(commitment, 0x4311, nonce),
+		"commitmment should not have verified",
 	)
 }
