daa35d2dde544e48c95fb9936aaf32f3ef0ba33e
Felix Prasanna <fpx@mit.edu>
2025-04-29 16:51:32 -0400

    Implement pederson commitments

--- commitmment/commitment_test.go
+++ commitmment/commitment_test.go
@@ -1,13 +1,17 @@
 package commitmment_test
 
 import (
-	// "testing"
+	"testing"
 
-	// "github.com/fprasx/secrets-and-spies/commitmment"
+	"github.com/fprasx/secrets-and-spies/commitmment"
+	"github.com/fprasx/secrets-and-spies/utils"
 )
 
-// func Test1(t *testing.T) {
-// 	message := uint(0xdecaf)
-// 	commitment, s := commitmment.Commit(message)
-// 	commitmment.MustValidateCommitment(commitment, message, s)
-// }
+func Test1(t *testing.T) {
+	message := uint(0xdecaf)
+	commitment, nonce := commitmment.Commit(message)
+	utils.Assert(
+		commitmment.Verify(commitment, message, nonce),
+		"failed to verify commitmment",
+	)
+}
